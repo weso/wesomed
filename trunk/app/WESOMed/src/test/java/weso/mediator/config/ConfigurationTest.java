871b62ddaf4c77acd65329a896eead4781a9e115
Jose Labra <labra@labra-Acer.(none)>
2012-08-27 16:34:08 +0200

    Some new tests

--- trunk/app/WESOMed/src/test/java/weso/mediator/config/ConfigurationTest.java
+++ trunk/app/WESOMed/src/test/java/weso/mediator/config/ConfigurationTest.java
@@ -2,14 +2,29 @@ package weso.mediator.config;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 public class ConfigurationTest {
 
 	@Test
 	public void testStopWordsFileName() {
-		String fileName = Configuration.getProperty("stop_wors_file.txt");
-		assertEquals(fileName,"es/stop_words.txt");
+		String fileName = Configuration.getProperty("stop_words_file");
+		assertEquals("es/stop_words.txt",fileName);
 	}
 
+	@Test
+	public void testStopWordsFile() {
+		String fileName = Configuration.getProperty("stop_words_file");
+		File directory = new File (".");
+		try {
+		System.out.println("Directory: " + directory.getCanonicalPath());
+		} catch(Exception e) {
+			System.out.println("Exceptione is ="+e.getMessage());
+		}
+		File file = new File(fileName);
+		assertTrue(file.exists());
+	}
 }
+
--- trunk/app/WESOMed/src/test/java/weso/mediator/util/UtilTest.java
+++ trunk/app/WESOMed/src/test/java/weso/mediator/util/UtilTest.java
@@ -13,15 +13,39 @@ import org.junit.Test;
 public class UtilTest {
 
 	@Test
-	public void testReadStopWords() {
+	public void spanishStopWordsContains_la() {
 		String fileName = Configuration.getProperty("stop_words_file");
 		try { 
 		  List <String> words = Util.readWords(fileName);
-		  assertTrue(words.contains("de"));
+		  assertTrue(words.contains("la"));
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Exception " + e);
 		}
 	}
 
+	@Test
+	public void spanishStopWordsContains_accented_que() {
+		String fileName = Configuration.getProperty("stop_words_file");
+		try { 
+		  List <String> words = Util.readWords(fileName);
+		  assertTrue(words.contains("qué"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception " + e);
+		}
+	}
+
+
+	@Test
+	public void spanishStopWordsDoesntContain_Juan() {
+		String fileName = Configuration.getProperty("stop_words_file");
+		try { 
+		  List <String> words = Util.readWords(fileName);
+		  assertFalse(words.contains("Juan"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception " + e);
+		}
+	}
 }
